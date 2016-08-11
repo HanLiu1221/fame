4d187dbe64857e6d9d11cbfb8fca40f87491d641
unknown <hla180@CS-GRUVI-04.cs.sfu.ca>
2016-08-10 17:29:30 -0700

    placeholder for human pose

---
+++ fameBase/Component/HumanPose.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Geometry;
+
+namespace Component
+{
+    public class HumanPose
+    {
+
+    }// HumanPose
+
+    class BodyNode
+    {
+        string _name;
+        Vector3d _originPos;
+        Vector3d _pos;
+        bool _isRoot = false; // move all nodes together
+        List<BodyNode> _adjNodes;
+        List<BodyNode> _childrenNodes;
+
+        public BodyNode(string name, Vector3d v)
+        {
+            _name = name;
+            _pos = new Vector3d(v);
+            _originPos = new Vector3d(v);
+        }
+    }// BodyNode
+
+    class BodyBone
+    {
+        BodyNode src;
+        BodyNode dst;
+        double radius; // of the cyclinder
+    }// BodyBone
+}
